0301375f24f89d013bae2475897ebb974c831815
dustinpfister <dustin.pfister@gmail.com>
2018-01-16 12:12:47 -0500

    making data files for each post

--- index.js
+++ index.js
@@ -4,16 +4,59 @@ generate a report on posts
 
  */
 
+// should come with hexo
 let cheerio = require('cheerio'),
 _ = require('lodash'),
-path = require('path');
+path = require('path'),
+
+// added with hexo-lexter package.json
+fs = require('fs-extra'),
+natural = require('natural'),
+
+tokenizer = new natural.WordTokenizer();
+
+// make a data file for the given post
+mkDataFile = function (post) {
+
+    let dir_data = path.join(hexo.base_dir, 'source', '_data'),
+    fileName = 'lexter-' + post.slug + '.json',
+    dir = path.join(dir_data, fileName),
+
+    $ = cheerio.load(post.content),
+
+    data = {};
+
+    data.tokens_p = tokenizer.tokenize($('p').text());
+    data.wc = data.tokens_p.length;
+
+    let json = JSON.stringify(data);
+
+    // start by ensuring the _data folder in source
+    return fs.ensureDir(dir_data).then(function () {
+
+        // write data file
+        return fs.writeFile(dir, json, 'utf-8');
+
+    }).then(function (json) {
+
+        console.log('success with: ' + fileName);
+
+    }).catch (function (e) {
+
+        console.log(e.message);
+
+    });
+
+};
 
 // tabulate word count for the given content, and element
-let tabWC = function (site,content, el) {
+let tabWC = function (site, content, el) {
 
     let $ = cheerio.load(content),
     text = $(el).text(),
-    count = text.split(' ').length;
+    //count = text.split(' ').length;
+
+    count = tokenizer.tokenize(text).length;
 
     if (text.length > 0) {
 
@@ -49,16 +92,18 @@ hexo.extend.generator.register('report', function (locals) {
     // to create paths on a for post basis
     let posts = locals.posts.map(function (post) {
 
+            mkDataFile(post);
+
             // tabulate site word count totals
             let ct = {
                 total: 0
             };
-            ct.p = tabWC(site,post.content, 'p');
+            ct.p = tabWC(site, post.content, 'p');
             ct.total += ct.p
 
             let h = 1;
             while (h < 7) {
-                ct['h' + h] = tabWC(post.content, 'h' + h);
+                ct['h' + h] = tabWC(site, post.content, 'h' + h);
                 ct.total += ct['h' + h];
                 h += 1;
             }
